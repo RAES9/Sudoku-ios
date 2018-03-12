38db5f671dbe76624ecc7197974080fd29ff3257
RAES <erivas@researchmobile.co>
2018-03-12 09:14:56 -0600

    Initial Commit

---
+++ SuDoKus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuDoKus
+//
+//  Created by Research Mobile on 12/03/18.
+//  Copyright © 2018 Research Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
